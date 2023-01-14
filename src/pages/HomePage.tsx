bdc0759c67f93675da52722783772b14813c63ee
RostislavOlsak <rosta.olsak12@gmail.com>
2023-01-14 16:01:04 +0100

    added edit function

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { getAuth, signOut } from "firebase/auth";
-import edit from "../Icons/edit-xxl.png"
+import editPhoto from "../Icons/editPhoto.png";
 
 const HomePage: React.FC = () => {
   const auth = getAuth();
@@ -8,6 +8,7 @@ const HomePage: React.FC = () => {
 
   const [taskText, setTaskText] = useState("");
   const [tasks, setTasks] = useState<string[]>([]);
+  const [editingTaskIndex, setEditingTaskIndex] = useState(-1);
 
   const addTask = (text: string) => {
     setTasks([...tasks, text]);
@@ -27,6 +28,13 @@ const HomePage: React.FC = () => {
   const deleteTodo = (index: number) => {
     const updatedTasks = tasks.filter((task, i) => i !== index);
     setTasks(updatedTasks);
+  };
+
+  const editTask = (index: number, newTaskText: string) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = newTaskText;
+    setTasks(updatedTasks);
+    setEditingTaskIndex(-1);
   };
 
   return (
@@ -60,12 +68,31 @@ const HomePage: React.FC = () => {
             <div className="tasks-input" key={index}>
               <div className="checkbox-with-text">
                 <input type="checkbox" className="todo-input" />
-                <label htmlFor="todo-input" data-content={task}>
-                  {task}
-                </label>
+                {index === editingTaskIndex ? (
+                  <div className="edit-container">
+                    <input
+                      type="text"
+                      value={taskText}
+                      onChange={(e) => setTaskText(e.target.value)}
+                      className="input-edit"
+                    />
+                    <button onClick={() => editTask(index, taskText)}>
+                      Save
+                    </button>
+                  </div>
+                ) : (
+                  <label htmlFor="todo-input" data-content={task}>
+                    {task}
+                  </label>
+                )}
               </div>
               <div className="buttons-container">
-              <button className="edit-button"><img className="img-edit" src={edit} /></button>
+                <button
+                  className="edit-button"
+                  onClick={() => setEditingTaskIndex(index)}
+                >
+                  <img className="img-edit" src={editPhoto} />
+                </button>
                 <button
                   onClick={() => deleteTodo(index)}
                   className="delete-button"
